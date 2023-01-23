b80a98edbbed746f615066dd46567db5448b27e8
Oscar Kevin <kurts_intermont@simplelogin.com>
2023-01-23 11:56:58 -0500

    Week 3 Updates (#2)
    * api updates
    
    * create world
    
    * get world by id
    
    * get worlds by status
    
    * delete world by object id
    
    * update document
    
    * update comments
    
    * return empty arrays

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"strconv"
 	"time"
 	"github.com/gorilla/mux"
+	"github.com/jinzhu/copier"
 	"github.com/rs/cors"
 	"github.com/spf13/viper"
 	"go.mongodb.org/mongo-driver/bson"
@@ -18,6 +19,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+/*** Structs ***/
 type Article struct {
 	Id      string `json:"Id"`
 	Title   string `json:"Title"`
@@ -38,19 +40,41 @@ type NameID struct {
 	World string
 }
 
-// let's declare a global Articles array
-// that we can then populate in our main function
-// to simulate a database
-var Articles []Article
-var DatabaseName		string
-var CollectionNames string 
-var MongoDbUser 		string
-var MongoDbPass 		string
-var MongoDbHost 		string
-var MongoDbPort 		int
-var ServerPort 			string
+type World struct {
+	Desc		string `json:"desc"`  // Text Area, User Input
+	Image		string `json:"image"` // URL, Created by Image Upload API
+	Status 	string `json:"status"`// Set to Private by Default
+	Owner 	string `json:"owner"` // Inserted by Auth
+	Title		string `json:"title"` // Text Field, User Input
+	Type 		string `json:"type"`  // Radio Select, User Input
+}
 
-/* Get MongoDB Client */
+type WorldID struct {
+	// World
+	Desc		string 	`json:"desc"` 
+	Image		string 	`json:"image"`
+	Owner 	string 	`json:"owner"`
+	Title		string 	`json:"title"`
+	Type 		string 	`json:"type"`
+	Status 	string 	`json:"status"`
+	ID	primitive.ObjectID `bson:"_id"`
+}
+
+// Global Variables
+var Articles []Article
+var CharacterLimit			int
+var DatabaseName 				string
+var DescriptionLimit		int
+var CollectionNames 		string 
+var CollectionWorlds 		string
+var MongoDbUser 				string
+var MongoDbPass 				string
+var MongoDbHost 				string
+var MongoDbPort 				int
+var ServerPort 					string
+
+/*--- MongoDB Helper Funcs ---*/
+// Get MongoDB Client
 func getMongoDBClient() (*mongo.Client, error) {
 	// mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb][?options]]
 	var mongoDbUri = fmt.Sprintf("mongodb://%s:%s@%s:%d", MongoDbUser, MongoDbPass, MongoDbHost, MongoDbPort)
@@ -61,20 +85,22 @@ func getMongoDBClient() (*mongo.Client, error) {
   return client, nil
 }
 
+/*--- REST Calls ---*/
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
 }
 
+// POST "/name"
 func createNewName(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: createNewName")
 	/* Umarshal POST body into Name struct */
 	reqBody, _ := ioutil.ReadAll(r.Body)
 	var name Name 
 	json.Unmarshal(reqBody, &name)
-	charLimit := viper.GetInt("CHARACTER_LIMIT")  // first 20 characters 
-	if len(name.Value) > 20 { name.Value = name.Value[: + charLimit] }
-	if len(name.World) > 20 { name.World = name.World[: + charLimit] }
+	CharacterLimit := viper.GetInt("CHARACTER_LIMIT")  // first 20 characters 
+	if len(name.Value) > CharacterLimit { name.Value = name.Value[: + CharacterLimit] }
+	if len(name.World) > CharacterLimit { name.World = name.World[: + CharacterLimit] }
 
 	/* Connect to database */
 	client, err := getMongoDBClient()
@@ -84,6 +110,7 @@ func createNewName(w http.ResponseWriter, r *http.Request) {
 			log.Fatal(err)
 	}
 	defer client.Disconnect(ctx)
+	fmt.Println(DatabaseName)
 	database := client.Database(DatabaseName)
 	collection := database.Collection(CollectionNames)
 
@@ -108,6 +135,44 @@ func createNewName(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(name)
 }
 
+// POST "/world"
+func createWorld(w http.ResponseWriter, r *http.Request) {
+	// Enter Func
+	fmt.Println("Endpoint Hit: createWorld")
+	
+	/* Umarshal POST body into struct */
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var world World 
+	json.Unmarshal(reqBody, &world)
+	if len(world.Desc) > DescriptionLimit { world.Desc = world.Desc[: + DescriptionLimit] }
+	if len(world.Title) > CharacterLimit { world.Title = world.Title[: + CharacterLimit] }
+
+	/* Connect to database */
+	client, err := getMongoDBClient()
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+			log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+	collection := client.Database(DatabaseName).Collection(CollectionWorlds)
+
+	// Insert New Doc
+	result, err := collection.InsertOne(ctx, world)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(result)
+
+	// Print Return Statement
+	var worldID WorldID
+	copier.Copy(&worldID, &world)
+	id := result.InsertedID.(primitive.ObjectID) //.Hex()
+	worldID.ID = id
+	fmt.Println(worldID)
+	json.NewEncoder(w).Encode(worldID)
+}
+
 /* DELETE /name/{tokenid} */
 func deleteNamebyTokenId(w http.ResponseWriter, r *http.Request){
 	fmt.Println("Endpoint Hit: deleteNamebyTokenId")
@@ -116,7 +181,7 @@ func deleteNamebyTokenId(w http.ResponseWriter, r *http.Request){
 	tokenid, err := strconv.Atoi(tokenIdStr)
 	fmt.Println(tokenIdStr)
 
-	/* Connect to our DB */
+	/* Connect to DB */
 	client, err := getMongoDBClient()
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	err = client.Connect(ctx)
@@ -133,6 +198,36 @@ func deleteNamebyTokenId(w http.ResponseWriter, r *http.Request){
 	msg := fmt.Sprintf("Deleted %v documents in the people collection", deleteResult.DeletedCount)
 	fmt.Fprintf(w, msg)
 	fmt.Println(msg)
+}
+
+// DELETE "/world/{id}"
+func deleteWorld(w http.ResponseWriter, r *http.Request){
+	// Start Func
+	fmt.Println("Endpoint Hit: deleteWorld")
+
+	// Get Path Params
+	vars := mux.Vars(r)
+	id, err := primitive.ObjectIDFromHex(vars["id"]) // ObjectID
+
+	/* Connect to DB */
+	client, err := getMongoDBClient()
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+			log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+
+	/* Get Document by Filter */
+	collection := client.Database(DatabaseName).Collection(CollectionWorlds)
+	filter := bson.D{{"_id", id}} 
+	result, err := collection.DeleteMany(context.TODO(), filter) 
+	if err != nil { log.Fatal(err) } 
+
+	// Return Result
+	msg := fmt.Sprintf("Deleted %v documents in the worlds collection", result.DeletedCount)
+	fmt.Println(msg)
+	fmt.Fprintf(w, msg)
 }
 
 /* GET /name/tokenid/{tokenid} */
@@ -197,6 +292,80 @@ func getNameByValue(w http.ResponseWriter, r *http.Request){
 	fmt.Println("Successfully returned name!")
 }
 
+/* GET /world/{id} */
+func getWorld(w http.ResponseWriter, r *http.Request){
+	// Start Func
+	fmt.Println("Endpoint Hit: getWorld")
+
+	// Get Path Params
+	vars := mux.Vars(r)
+	id, err := primitive.ObjectIDFromHex(vars["id"]) // ObjectID
+	
+	// Connect to our DB
+	client, err := getMongoDBClient()
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+			log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+
+	/* Get Document by Filter */
+	collection := client.Database(DatabaseName).Collection(CollectionWorlds)
+	filter := bson.M{"_id": id}
+	var result WorldID
+	err = collection.FindOne(context.TODO(), filter).Decode(&result)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result)
+
+	// Return Results
+	json.NewEncoder(w).Encode(result)
+	fmt.Println("Successfully returned world!")
+}
+
+func getWorlds(w http.ResponseWriter, r *http.Request) {
+	// Start Func
+	fmt.Println("Endpoint Hit: getWorlds")
+
+	// Get Query Params
+	params := r.URL.Query()
+	fmt.Println(params)
+	var status string
+	if _, ok := params["status"]; ok { 
+		status = params["status"][0]
+	} else {
+		status = "public" 
+	}
+
+	/* Connect to our DB */
+	client, err := getMongoDBClient()
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+			log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+
+	/* Get All Documents in Collection By Filter */
+	collection := client.Database(DatabaseName).Collection(CollectionWorlds)
+	filter := bson.D{{"status", status}}
+	cursor, err := collection.Find(ctx, filter, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var results []bson.M
+	if err = cursor.All(ctx, &results); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(results)
+
+	// Return Results
+	json.NewEncoder(w).Encode(results)
+	fmt.Println("Successfully returned worlds from status %s!", status)
+}
+
 func returnAllNamesByWorld(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: returnAllNamesByWorld")
 	vars := mux.Vars(r)
@@ -224,7 +393,12 @@ func returnAllNamesByWorld(w http.ResponseWriter, r *http.Request) {
     log.Fatal(err)
 	}
 	fmt.Println(results)
-	json.NewEncoder(w).Encode(results)
+	if results == nil {
+		emptyArray := []string{}
+		json.NewEncoder(w).Encode(emptyArray)
+	} else {
+		json.NewEncoder(w).Encode(results)
+	}
 	fmt.Println("Successfully returned names from world %s!", world)
 }
 
@@ -275,6 +449,62 @@ func updateExistingName(w http.ResponseWriter, r *http.Request){
 	fmt.Println(updatedName)
 }
 
+// PUT "/world"
+func updateWorld(w http.ResponseWriter, r *http.Request){
+	// Start Func
+	fmt.Println("Endpoint Hit: updateWorld")
+
+	// Unmarshal Request Body
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var world WorldID 
+	json.Unmarshal(reqBody, &world)
+	fmt.Println("---world")
+	fmt.Println(world)
+
+	// Connect to DB
+	client, err := getMongoDBClient()
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+			log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+
+	// Create Updated Document
+	update := bson.D{
+		{"$set", 
+			bson.D{
+				{"desc", 		world.Desc},
+				{"image", 	world.Image},
+				{"owner", 	world.Owner},
+				{"title", 	world.Title},
+				{"type", 		world.Type},
+				{"status", 	world.Status},
+			},
+		},
+	}
+	fmt.Println("---update")
+	fmt.Println(update)
+
+	// Update Document by Id
+	collection := client.Database(DatabaseName).Collection(CollectionWorlds)
+	filter := bson.M{"_id": world.ID}
+	result, err := collection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("---result")
+	fmt.Println(result)
+
+	// Return Statement
+	if result.ModifiedCount > 0 {
+		json.NewEncoder(w).Encode(world)
+		fmt.Println(world)
+	} else {
+		fmt.Fprintf(w, "Unable to update record")
+		fmt.Println("Unable to update record")
+	}
+}
 
 // REST Endpoints
 func handleRequests() {
@@ -291,6 +521,14 @@ func handleRequests() {
 	myRouter.HandleFunc("/name/{tokenid}", deleteNamebyTokenId).Methods("DELETE")
 	myRouter.HandleFunc("/name", createNewName).Methods("POST")
 	myRouter.HandleFunc("/name", updateExistingName).Methods("PUT")
+
+	/* Worlds */
+	myRouter.HandleFunc("/worlds", getWorlds).Methods("GET") // By Status
+	myRouter.HandleFunc("/world", createWorld).Methods("POST")
+	myRouter.HandleFunc("/world", updateWorld).Methods("PUT")
+	myRouter.HandleFunc("/world/{id}", getWorld).Methods("GET") // By Object ID
+	myRouter.HandleFunc("/world/{id}", deleteWorld).Methods("DELETE") // By Object ID
+
 
 	/* Set up CORS */
 	// c := cors.New(cors.Options{
@@ -323,8 +561,11 @@ func main() {
 	MongoDbPass = viper.GetString("mongodb.pass")
 	MongoDbHost = viper.GetString("mongodb.host")
 	MongoDbPort = viper.GetInt("mongodb.port")
-	DatabaseName = viper.GetString("DATABASE_NAME") 
+	DescriptionLimit = viper.GetInt("DESCRIPTION_LIMIT")
+	CharacterLimit = viper.GetInt("CHARACTER_LIMIT")
 	CollectionNames = viper.GetString("COLLECTION_NAMES") 
+	CollectionWorlds = viper.GetString("COLLECTION_WORLDS")
+	DatabaseName = viper.GetString("DATABASE_NAME") 
 
 	// Start up server
 	fmt.Println("Starting server...")
